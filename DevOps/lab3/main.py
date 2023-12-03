43277a2ff432f7bbe8dee28f1f9e92c854bf0aff
hellkirl <kirillprivalov04@icloud.com>
2023-12-04 00:04:04 +0300

    build: add lab-3

--- DevOps/lab3/main.py
+++ DevOps/lab3/main.py
@@ -2,18 +2,11 @@ import json
 import requests
 from datetime import datetime
 import re
-import os
 
-files_directory = 'files'
-
-all_files = sorted(os.listdir(files_directory), key=lambda x: os.path.getmtime(os.path.join(files_directory, x)),
-                   reverse=True)
-
-latest_file = all_files[0] if all_files else None
-
-if latest_file:
-    with open(os.path.join(files_directory, latest_file), 'r', encoding='utf-8') as f:
-        data = json.load(f)
+try:
+    with open('files/main.json', 'r', encoding='utf-8') as f:
+        data = json.load(
+            f)
         if data['link']:
             response = requests.get(data['link'])
 
@@ -34,5 +27,6 @@ if latest_file:
             datetime=datetime.now().strftime('%d.%m.%Y-%H:%M:%S'),
         )
         json.dump(d, t, ensure_ascii=False, indent=2)
-else:
-    print("No files found in the specified directory.")
+except Exception as err:
+    with open(f'{err}-{datetime.now().strftime("%d.%m.%Y-%H:%M:%S")}.json', 'a', encoding='utf-8'):
+        d = dict(error=str(err), datetime=datetime.now().strftime("%d.%m.%Y-%H:%M:%S"))
