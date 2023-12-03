9f42a7d6e76b9e4ee556d4576f1c3b45eb08a8b1
hellkirl <kirillprivalov04@icloud.com>
2023-12-04 01:04:11 +0300

    build: add lab-3

--- DevOps/lab3/main.py
+++ DevOps/lab3/main.py
@@ -1,25 +1,19 @@
 import json
 import requests
 from datetime import datetime
-import re
 
 try:
     with open('files/main.json', 'r', encoding='utf-8') as f:
-        data = json.load(
-            f)
+        data = json.load(f)
         if data['link']:
             response = requests.get(data['link'])
 
-    link = re.search(r'(\w+\.\w+)', data['link']).group(0)
-
-    filename = (
-        f'reports/output.json'
-    )
+    filename = f'reports/output.json'
 
     with open(
-            filename,
-            'a',
-            encoding='utf-8',
+        filename,
+        'a',
+        encoding='utf-8',
     ) as t:
         d = dict(
             url=response.url,
@@ -28,5 +22,8 @@ try:
         )
         json.dump(d, t, ensure_ascii=False, indent=2)
 except Exception as err:
-    with open(f'{err}-{datetime.now().strftime("%d.%m.%Y")}-{datetime.now().timestamp()}-.json', 'a', encoding='utf-8'):
-        d = dict(error=str(err), datetime=datetime.now().strftime("%d.%m.%Y-%H:%M:%S"))
+    with open(f'output.json', 'a', encoding='utf-8'):
+        d = dict(
+            error=str(err),
+            datetime=datetime.now().strftime('%d.%m.%Y-%H:%M:%S'),
+        )
